1255655f5a9c87ea6851987425c80aa369aa7ced
Saad Zulfiqar <szulfiqar@xavor.com>
2021-08-05 21:59:05 +0500

    Fifth commit

--- src/Components/Edwards/Accordian.js
+++ src/Components/Edwards/Accordian.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Accordion,
     AccordionItem,
@@ -12,14 +12,14 @@ import { translate } from 'react-translate';
 
 const Accordian = props => {
     const { data } = props;
+    const [isExpandAll, setExpandAll] = useState(true);
     return (
-        <div>
-            <span class="glyphicon expand-plus-sign"></span>
-
+        <div className="col-lg-12">
+            <span className="glyphicon expand-plus-sign" onClick={() => setExpandAll(!isExpandAll)}></span>
             <Accordion allowMultipleExpanded={true}>
                 {
                     data.map((accordian, index) => {
-                        return <AccordionItem key={index} uuid={index}>
+                        return <AccordionItem key={index} dangerouslySetExpanded={isExpandAll}>
                             <AccordionItemHeading>
                                 <AccordionItemButton>
                                     {accordian.title.value}
